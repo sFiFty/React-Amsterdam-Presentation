19dd4709448a7fc7668d41302181fa8ebdb56815
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-05-21 07:29:10 +0300

    added strict mode component

--- react-amsterdam/src/components/StrictMode/StrictMode.js
+++ react-amsterdam/src/components/StrictMode/StrictMode.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, StrictMode } from 'react';
 
-class StrictMode extends Component {
+class StrictModeWrapper extends Component {
   render() {
     return (
-      <div className="strict-mode">
-        StrictMode
+      <StrictMode>
+        <InnerStrictMode />
+      </StrictMode>
+    );
+  }
+}
+class InnerStrictMode extends Component {
+
+  componentWillMount() {
+
+  }
+
+  componentWillUpdate() {
+
+  }
+
+  componentWillReceiveProps() {
+    
+  }
+
+  render() {
+    return (
+      <StrictMode>
+        <div ref="ref" className="strict-mode">
+        <h3>StrictMode currently helps with:</h3>
+        <ul className="list-group">
+          <li className="list-group-item">Identifying components with unsafe lifecycles</li>
+          <li className="list-group-item">Warning about legacy string ref API usage</li>
+          <li className="list-group-item">Detecting unexpected side effects</li>
+        </ul>
       </div>
+      </StrictMode>
     );
   }
 }
 
-export default StrictMode;
+export default StrictModeWrapper;
